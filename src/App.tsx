1fb623da0fa439af4f57ad89d6a626754d72bd3f
ali <mehaboobali@capitalnumbers.com>
2021-05-31 11:35:38 +0530

    added user Details action Thunk

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { useDispatch } from "react-redux";
 import "./App.css";
 import { AppDispatch, useAppSelector } from "./redux/store";
-import { decrement, increment, incrementByValue } from "./redux/reducer";
+import {
+  decrement,
+  increment,
+  incrementByValue,
+  getUser,
+} from "./redux/reducer";
 
 const App = () => {
-  const { value } = useAppSelector((state) => state.counter);
+  const { value, userData, loading } = useAppSelector((state) => state.counter);
   const dispatch = useDispatch<AppDispatch>();
+
   return (
     <div className="container">
       <h1>Count:{value}</h1>
@@ -15,6 +21,23 @@ const App = () => {
         <button onClick={() => dispatch(incrementByValue(5))}>
           increment by value 5
         </button>
+
+        <button onClick={() => dispatch(getUser())}>Fetch User</button>
+      </div>
+
+      <div className="user__data_wrapper">
+        {loading ? (
+          <h2>Loading...</h2>
+        ) : (
+          userData && (
+            <div className="user__data_user__details">
+              <p>Name: {userData.name}</p>
+              <p>Email: {userData.email}</p>
+              <p>Phone: {userData.phone}</p>
+              <p>Website: {userData.website}</p>
+            </div>
+          )
+        )}
       </div>
     </div>
   );
--- src/redux/reducer.ts
+++ src/redux/reducer.ts
@@ -1,10 +1,51 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction, createAsyncThunk } from "@reduxjs/toolkit";
+
+export interface Object {
+  [key: string]: unknown;
+}
+
+export interface UserData {
+  address: Object;
+  company: Object;
+  email: string;
+  id: number;
+  name: string;
+  phone: string;
+  username: string;
+  website: string;
+}
+
+export interface FetchTodoError {
+  message: string;
+}
+
+export interface State {
+  value: number;
+  userData: UserData | null;
+  loading: boolean;
+}
+
+const initialState: State = {
+  value: 0,
+  userData: null,
+  loading: false,
+};
+
+export const getUser = createAsyncThunk<UserData>("getTodo", async () => {
+  let userId = Math.floor(Math.random() * 10);
+  if (userId === 0) {
+    userId = 1;
+  }
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/users/${userId}`
+  );
+  const res2 = await res.json();
+  return res2;
+});
 
 const counter = createSlice({
   name: "counter",
-  initialState: {
-    value: 0,
-  },
+  initialState,
   reducers: {
     increment: (state) => {
       state.value++;
@@ -17,6 +58,18 @@ const counter = createSlice({
     incrementByValue: (state, action: PayloadAction<number>) => {
       state.value += action.payload;
     },
+  },
+  extraReducers: (bundler) => {
+    bundler.addCase(getUser.pending, (state) => {
+      state.loading = true;
+    });
+    bundler.addCase(
+      getUser.fulfilled,
+      (state, action: PayloadAction<UserData>) => {
+        state.loading = false;
+        state.userData = action.payload;
+      }
+    );
   },
 });
 
